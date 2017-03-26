20d765f615a6e2853f9d986bc68c36765601f40c
Corey Deli <iwifia@live.com>
2017-03-25 20:42:53 -0400

    Text Changes

--- CreatedThisWeek.ps1
+++ CreatedThisWeek.ps1
@@ -6,15 +6,16 @@
 # Purpose: Send admin email for all new #
 # and groups created within last week.  #
 #                                       #
-# Version: 1.0                          #
+# Version: 1.1                          #
 #########################################
 
 $week = (Get-Date).AddDays(-7)
 $today = (Get-Date).ToString()
 
 # HTML to make the design pretty and not cluttered
-$a = $a + "<style>BODYBODY{background-color:Lavender ;}TABLE{border-width: 1px;border-style: solid;border-color: black;border-collapse: collapse;}TH{border-width: 1px;padding: 5px;border-style: solid;border-color: black;background-color:thistle}TD{border-width: 1px;padding: 5px;border-style: solid;border-color: black;background-color:PaleGoldenrod} 
-</style><!--mce:0-->"
+$a = $a + "<style>BODYBODY{background-color:Lavender ;}TABLE{border-width: 1px;border-style: solid;border-color: black;border-collapse: collapse;}`
+TH{border-width: 1px;padding: 5px;border-style: solid;border-color: black;background-color:thistle}TD{border-width: 1px;padding: 5px;border-style: solid;`
+border-color: black;background-color:PaleGoldenrod} </style><!--mce:0-->"
 
 # Settings for email
 $smtp = "mail.contoso.com"
@@ -29,11 +30,11 @@ Import-Module ActiveDirectory
 # Run report on users created in the last week
 $Users = Get-ADUser -Filter * -Properties * | Where-Object { $_.whenCreated -ge $week } | Sort-Object | `
 Select-Object Name,EmployeeID,Title,Department,Mail,physicalDeliveryOfficeName,StreetAddress,City,State,PostalCode,TelephoneNumber,Mobile,whenCreated `
-| ConvertTo-Html -Head $a -Body "<H2>Users created this week.</H2>"
+| ConvertTo-Html -Head $a -Body "<H2>Users that have been created during the week of $week.</H2>"
 
 # Run report on all groups created in the last week
 $group = Get-ADGroup -Filter * -Properties * | Where-Object { $_.whenCreated -ge $week } | Sort-Object | Select-Object Name,Mail,Description,whenCreated `
-| ConvertTo-Html -Head $a -Body "<H2>Groups created this week.</H2>"
+| ConvertTo-Html -Head $a -Body "<H2>Groups that have been created during the week of $week.</H2>"
 
 $body = "Creation period from $week to $today ." 
 $body += "`n" 
